515d22996f990b1b1d992fcf339c85b8551b8ae0
ZAslam93 <ZAslam93@users.noreply.github.com>
2020-11-18 22:42:32 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -137,7 +137,7 @@ bool vote(int voter, int rank, string name)
         
         if (strcmp(name, candidates[i].name) == 0)
         {
-            preferences[voter][rank] = i;ZA
+            preferences[voter][rank] = i;
             return true;
         }
     }
