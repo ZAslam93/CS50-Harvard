df0116ea7b4c67bdf6396aa79fcfdbcff01995c1
ZAslam93 <ZAslam93@users.noreply.github.com>
2020-11-24 23:58:58 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -11,7 +11,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Computing the shade of grey for each pixel
-            greyvalue = (int) round((image[i][j].rgbtBlue + image[i][j].rgbtGreen +
+            greyvalue = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen +
             image[i][j].rgbtRed) / 3);
 
             // Setting each pixel to equal shade
