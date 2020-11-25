9c6abb229302e28e1ddcc0eccf2460af25db3b79
ZAslam93 <ZAslam93@users.noreply.github.com>
2020-11-25 23:05:47 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,5 +1,5 @@
 #include "helpers.h"
-// Including header math.h for rounding functionality
+// Including header math.h for rounding functionality, bmp.h for RGBTRIPLE?
 #include <math.h>
 
 // Convert image to grayscale --function COMPLETE--
@@ -12,8 +12,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Computing the shade of grey for each pixel
-            greyvalue = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen +
-            image[i][j].rgbtRed) / 3.0);
+            greyvalue = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
             // Setting each pixel to equal shade
             image[i][j].rgbtBlue = greyvalue;
             image[i][j].rgbtGreen = greyvalue;
@@ -23,7 +22,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
-// Convert image to sepia
+// Convert image to sepia --function COMPLETE--
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
     // Initializing [spBlue] [spGreen] [spRed]
@@ -35,12 +34,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Calculating each pixel's sepia value using formula
-            spBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen
-            + .131 * image[i][j].rgbtBlue);
-            spGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen
-            + .168 * image[i][j].rgbtBlue);
-            spRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen
-            + .189 * image[i][j].rgbtBlue);
+            spBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+            spGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+            spRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
 
             // Setting maximum sepia value to 255
             if (spBlue > 255)
@@ -68,7 +64,21 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // Initializing [temp]
+    RGBTRIPLE temp[height][width];
+    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < (int) round(width /2); j++)
+        {
+            
+            // Swapping pixel values using temp
+            temp[i][j] = image[i][j];
+            image[i][j] = image[i][width - (j + 1)];
+            image[i][width - (j + 1)] = temp[i][j];
+
+        }
+    }
 }
 
 // Blur image
