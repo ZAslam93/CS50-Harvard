156a4e87dc684227786ec5c00bf9699dc1368652
ZAslam93 <ZAslam93@users.noreply.github.com>
2021-01-28 20:15:08 +0000

    automated commit by check50 [check50=True]

--- 11.sql
+++ 11.sql
@@ -5,5 +5,6 @@ ON ratings.movie_id = movies.id
 WHERE movies.id IN
 (SELECT movie_id FROM stars WHERE person_id =
 (SELECT id FROM people WHERE name = 'Chadwick Boseman'))
-ORDER BY ratings.rating
+ORDER BY ratings.rating DESC
 LIMIT 5;
+
