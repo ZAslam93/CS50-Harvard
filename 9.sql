f5f092598352d838372de4482a9895d712647ba0
ZAslam93 <ZAslam93@users.noreply.github.com>
2021-01-28 18:04:14 +0000

    automated commit by check50 [check50=True]

--- 9.sql
+++ 9.sql
@@ -2,4 +2,6 @@ SELECT DISTINCT name FROM people WHERE id IN
 (SELECT person_id FROM stars WHERE movie_id IN
 (SELECT id FROM movies WHERE year = 2004))
 ORDER BY
-(SELECT birth FROM people) ASC;
+(SELECT DISTINCT name FROM people WHERE id IN
+(SELECT person_id FROM stars WHERE movie_id IN
+(SELECT id FROM movies WHERE year = 2004)));
