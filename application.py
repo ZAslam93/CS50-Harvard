86a07de8e6c34f61a7ad44b58e6dd2f850f3413b
ZAslam93 <ZAslam93@users.noreply.github.com>
2021-02-26 16:44:14 +0000

    automated commit by check50 [check50=True]

--- application.py
+++ application.py
@@ -94,7 +94,7 @@ def buy():
         if not buy_stock:
             return apology("Invalid stock symbol")
 
-        if not request.form.get("shares").isnumeric() and request.form.get("shares") > 0:
+        if not int(request.form.get("shares")).isnumeric() and int(request.form.get("shares")) > 0:
             return apology("Invalid shares")
 
         # Formatting cash and purchase values
